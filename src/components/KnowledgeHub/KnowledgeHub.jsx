53a524a49b49049f23251d21b7af745707da2b9d
Ashwoq_s_ <ashwoq00786@gmail.com>
2024-08-10 10:23:32 +0530

    knowledge

--- src/components/KnowledgeHub/KnowledgeHub.jsx
+++ src/components/KnowledgeHub/KnowledgeHub.jsx
@@ -1,37 +1,88 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import NewKnowledge from "./NewKnowledge";
 import Sidebar from "../global/Sidebar";
 import assets from "../../assets/assets";
 import MobileSidebar from "../global/MobileSidebar";
+import {
+  collection,
+  getDocs,
+  addDoc,
+  updateDoc,
+  doc,
+  deleteDoc,
+  onSnapshot,
+} from "@firebase/firestore";
+import { db } from "../../firebase.js";
 
 const KnowledgeHub = () => {
   const [openAddnewMentor, setopenAddnewMentor] = useState(false);
   const [openSidebar, setOpenSidebar] = useState(false);
+  const [edit, setedit] = useState(false);
+  const [defaultEditData, setDefaultEditData] = useState({
+    type: "",
+    sourceType: "",
+    description: "",
+    mediaFile: null,
+    mediaUrl: "",
+  });
+
+  const KnowledgeRef = collection(db, "knowledge");
+
+  const [knowledgeData, setKnowledgeData] = useState([]);
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(KnowledgeRef, (snapshot) => {
+      setKnowledgeData(
+        snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+      );
+    });
+
+    // Cleanup the listener on component unmount
+    return () => unsubscribe();
+  }, []);
+
+  const deleteEvent = async (id) => {
+    try {
+      if (window.confirm("Are you sure you want to delete this mentor?")) {
+        const docRef = doc(db, "knowledge", id);
+        await deleteDoc(docRef);
+        console.log("deleted");
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
   return (
-    <div className="w-screen h-screen bg-white flex ">
+    <div className="flex w-screen h-screen bg-white">
       <Sidebar />
-      <div className="dashboard-content bg-white p-4 relative overflow-x-scroll  ">
+      <div className="relative p-4 overflow-x-scroll bg-white dashboard-content">
         {openAddnewMentor && (
-          <NewKnowledge onCancel={() => setopenAddnewMentor(false)} />
+          <NewKnowledge
+            edit={edit}
+            setedit={setedit}
+            defaultEditData={defaultEditData}
+            setDefaultEditData={setDefaultEditData}
+            onCancel={() => setopenAddnewMentor(false)}
+          />
         )}
         {openSidebar && <MobileSidebar />}
         <div
-          className={`w-full h-20 flex items-center justify-between  ${
+          className={`w-full h-20 flex items-center justify-between ${
             openAddnewMentor && "blur-sm"
           }`}
         >
           <img
             src={assets.Img.Menu_Icon}
             alt="icon"
-            className="w-7 h-7 opacity-100 object-contain lg:opacity-0"
+            className="object-contain opacity-100 w-7 h-7 lg:opacity-0"
             onClick={() => setOpenSidebar(true)}
           />
-          <div className="w-fit h-full flex items-center gap-4">
+          <div className="flex items-center h-full gap-4 w-fit">
             <img
               src={assets.Img.Bg}
               alt="icon"
-              className="w-7 h-7 rounded-full object-cover"
+              className="object-cover rounded-full w-7 h-7"
             />
             <p className="text-[1.2rem] font-semibold text-blue-600">
               Test User
@@ -43,27 +94,27 @@ const KnowledgeHub = () => {
             openAddnewMentor && "blur-sm"
           }`}
         >
-          <div className="w-fit h-full">
-            <h1 className="font-bold text-[1.5rem] ">Knowledge Hub</h1>
+          <div className="h-full w-fit">
+            <h1 className="font-bold text-[1.5rem]">Knowledge Hub</h1>
             <p>Lorem ipsum dolor sit Pamet consectetur adipisicing elit.</p>
           </div>
         </div>
         <div
           className={`w-full h-[calc(100%-3rem)] bg-white mt-4 p-2 ${
             openAddnewMentor && "blur-sm"
-          } `}
+          }`}
         >
-          <div className="w-full h-10 flex justify-between items-center flex-wrap gap-4 mt-8 md:mt-0  ">
+          <div className="flex flex-wrap items-center justify-between w-full h-10 gap-4 mt-8 md:mt-0">
             <div className="bg-[#edededde] w-[20rem] h-[90%] px-2 flex items-center gap-1 border rounded-md text-white border-blue-200">
               <img
                 src={assets.Img.Search}
                 alt="search"
-                className="w-4 h-4 object-contain"
+                className="object-contain w-4 h-4"
               />
               <input
                 type="text"
                 placeholder="Find"
-                className="flex-1 outline-none border-none pl-2 bg-transparent placeholder-gray-400"
+                className="flex-1 pl-2 placeholder-gray-400 bg-transparent border-none outline-none"
               />
             </div>
             <div
@@ -73,8 +124,8 @@ const KnowledgeHub = () => {
               Add New
             </div>
           </div>
-          <div className="min-w-fit w-full h-[calc(100%-4rem)] bg-gray-50 mt-16  md:mt-4  rounded-lg flex flex-col gap-4 ">
-            <div className="w-full h-10 flex justify-between gap-4 bg-[#f4f4f4] px-4 rounded-t-md ">
+          <div className="min-w-fit w-full h-[calc(100%-4rem)] bg-gray-50 mt-16 md:mt-4 rounded-lg flex flex-col gap-4">
+            <div className="w-full h-10 flex justify-between gap-4 bg-[#f4f4f4] px-4 rounded-t-md">
               <div className="w-[2rem] h-full flex gap-2 items-center text-black font-medium">
                 <p>SI.No</p>
               </div>
@@ -84,7 +135,7 @@ const KnowledgeHub = () => {
               <div className="w-[8rem] h-full flex gap-2 items-center text-black font-medium">
                 <p>Source Type</p>
               </div>
-              <div className="w-[30rem] h-full flex gap-2 items-center text-black font-medium">
+              <div className="w-[30rem] h-full flex gap-4 items-center text-black font-medium">
                 <p>Description</p>
               </div>
               <div className="w-[8rem] h-full flex gap-2 items-center text-black font-medium">
@@ -94,132 +145,68 @@ const KnowledgeHub = () => {
                 <p>Action</p>
               </div>
             </div>
-            <div className="w-full h-12 flex justify-between px-4 items-center">
-              <div className="w-[2rem] h-fit flex gap-2 items-center text-gray-500">
-                <p>01</p>
-              </div>
-              <div className="w-[8rem] h-fit flex gap-2 items-center text-gray-500">
-                <p>11 dec 2024</p>
-              </div>
-              <div className="w-[8rem] h-fit flex gap-2 items-center text-gray-500">
-                <p>Image</p>
-              </div>
-              <div className="w-[30rem] h-full overflow-hidden flex gap-4 items-center text-gray-500">
-                <p className="w-full h-full text-[12px] text-justify ">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Cupiditate quo ipsa dignissimos, quos itaque rem quisquam
-                  distinctio, ...
-                </p>
-              </div>
-              <div className="w-[8rem] h-full flex gap-2 items-center text-gray-500">
-                <img
-                  src={assets.Img.Bg}
-                  alt="icon"
-                  className="w-full h-[90%] object-cover rounded-md"
-                />
-              </div>
-              <div className="w-[5rem] h-fit flex gap-1 items-center justify-between ">
-                <div className="w-[2rem] h-8  bg-gray-100 hover:bg-blue-100 rounded-md cursor-pointer flex items-center justify-center ">
+
+            {knowledgeData.map((knowledge, index) => (
+              <div
+                key={knowledge.id}
+                className="flex items-center justify-between w-full h-12 px-4"
+              >
+                <div className="w-[2rem] h-fit flex gap-2 items-center text-gray-500">
+                  <p>{index + 1}</p>
+                </div>
+                <div className="w-[8rem] h-fit flex gap-2 items-center text-gray-500">
+                  <p>{knowledge.type}</p>
+                </div>
+                <div className="w-[8rem] h-fit flex gap-2 items-center text-gray-500">
+                  <p>{knowledge.sourceType}</p>
+                </div>
+                <div className="w-[30rem] h-full overflow-hidden flex gap-4 items-center text-gray-500">
+                  <p className="w-full h-full text-[12px] text-justify">
+                    {knowledge.description}
+                  </p>
+                </div>
+                <div className="w-[8rem] h-full flex gap-2 items-center text-gray-500">
                   <img
-                    src={assets.Img.edit}
-                    onClick={() => setopenAddnewMentor(true)}
+                    src={knowledge.mediaFile}
                     alt="icon"
-                    className="w-[60%] h-[60%] object-contain"
+                    className="w-full h-[90%] object-cover rounded-md"
                   />
                 </div>
-                <div className="w-[2rem] h-8  cursor-pointer bg-gray-100 rounded-md hover:bg-red-100  flex items-center justify-center ">
-                  <img
-                    src={assets.Img.Delete}
-                    alt="icon"
-                    className="w-[60%] h-[60%] object-contain"
-                  />
+                <div className="w-[5rem] h-fit flex gap-1 items-center justify-between">
+                  <div
+                    className="w-[2rem] h-8 bg-gray-100 hover:bg-blue-100 rounded-md cursor-pointer flex items-center justify-center"
+                    onClick={() => {
+                      setopenAddnewMentor(true);
+                      setedit(true);
+                      setDefaultEditData({
+                        id: knowledge.id,
+                        type: knowledge.type,
+                        sourceType: knowledge.sourceType,
+                        mediaFile: knowledge.mediaFile,
+                        description: knowledge.description,
+                        mediaUrl: knowledge.mediaUrl,
+                      });
+                    }}
+                  >
+                    <img
+                      src={assets.Img.edit}
+                      alt="icon"
+                      className="w-[60%] h-[60%] object-contain"
+                    />
+                  </div>
+                  <div
+                    onClick={() => deleteEvent(knowledge.id)}
+                    className="w-[2rem] h-8 cursor-pointer bg-gray-100 rounded-md hover:bg-red-100 flex items-center justify-center"
+                  >
+                    <img
+                      src={assets.Img.Delete}
+                      alt="icon"
+                      className="w-[60%] h-[60%] object-contain"
+                    />
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="w-full h-12 flex justify-between px-4 items-center">
-              <div className="w-[2rem] h-fit flex gap-2 items-center text-gray-500">
-                <p>01</p>
-              </div>
-              <div className="w-[8rem] h-fit flex gap-2 items-center text-gray-500">
-                <p>11 dec 2024</p>
-              </div>
-              <div className="w-[8rem] h-fit flex gap-2 items-center text-gray-500">
-                <p>Image</p>
-              </div>
-              <div className="w-[30rem] h-full overflow-hidden flex gap-4 items-center text-gray-500">
-                <p className="w-full h-full text-[12px] text-justify ">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Cupiditate quo ipsa dignissimos, quos itaque rem quisquam
-                  distinctio, ...
-                </p>
-              </div>
-              <div className="w-[8rem] h-full flex gap-2 items-center text-gray-500">
-                <img
-                  src={assets.Img.Bg}
-                  alt="icon"
-                  className="w-full h-[90%] object-cover rounded-md"
-                />
-              </div>
-              <div className="w-[5rem] h-fit flex gap-1 items-center justify-between ">
-                <div className="w-[2rem] h-8  bg-gray-100 hover:bg-blue-100 rounded-md cursor-pointer flex items-center justify-center ">
-                  <img
-                    src={assets.Img.edit}
-                    onClick={() => setopenAddnewMentor(true)}
-                    alt="icon"
-                    className="w-[60%] h-[60%] object-contain"
-                  />
-                </div>
-                <div className="w-[2rem] h-8  cursor-pointer bg-gray-100 rounded-md hover:bg-red-100  flex items-center justify-center ">
-                  <img
-                    src={assets.Img.Delete}
-                    alt="icon"
-                    className="w-[60%] h-[60%] object-contain"
-                  />
-                </div>
-              </div>
-            </div>
-            <div className="w-full h-12 flex justify-between px-4 items-center">
-              <div className="w-[2rem] h-fit flex gap-2 items-center text-gray-500">
-                <p>01</p>
-              </div>
-              <div className="w-[8rem] h-fit flex gap-2 items-center text-gray-500">
-                <p>11 dec 2024</p>
-              </div>
-              <div className="w-[8rem] h-fit flex gap-2 items-center text-gray-500">
-                <p>Image</p>
-              </div>
-              <div className="w-[30rem] h-full overflow-hidden flex gap-4 items-center text-gray-500">
-                <p className="w-full h-full text-[12px] text-justify ">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Cupiditate quo ipsa dignissimos, quos itaque rem quisquam
-                  distinctio, ...
-                </p>
-              </div>
-              <div className="w-[8rem] h-full flex gap-2 items-center text-gray-500">
-                <img
-                  src={assets.Img.Bg}
-                  alt="icon"
-                  className="w-full h-[90%] object-cover rounded-md"
-                />
-              </div>
-              <div className="w-[5rem] h-fit flex gap-1 items-center justify-between ">
-                <div className="w-[2rem] h-8  bg-gray-100 hover:bg-blue-100 rounded-md cursor-pointer flex items-center justify-center ">
-                  <img
-                    src={assets.Img.edit}
-                    onClick={() => setopenAddnewMentor(true)}
-                    alt="icon"
-                    className="w-[60%] h-[60%] object-contain"
-                  />
-                </div>
-                <div className="w-[2rem] h-8  cursor-pointer bg-gray-100 rounded-md hover:bg-red-100  flex items-center justify-center ">
-                  <img
-                    src={assets.Img.Delete}
-                    alt="icon"
-                    className="w-[60%] h-[60%] object-contain"
-                  />
-                </div>
-              </div>
-            </div>
+            ))}
           </div>
         </div>
       </div>
--- src/components/KnowledgeHub/NewKnowledge.jsx
+++ src/components/KnowledgeHub/NewKnowledge.jsx
@@ -1,6 +1,23 @@
+import { uploadBytes } from "firebase/storage";
 import React, { useEffect, useState } from "react";
+import {
+  collection,
+  getDocs,
+  addDoc,
+  updateDoc,
+  doc,
+  deleteDoc,
+} from "@firebase/firestore";
+import { db, storage } from "../../firebase.js";
+import { getDownloadURL, ref } from "firebase/storage";
 
-const NewKnowledge = ({ onCancel }) => {
+const NewKnowledge = ({
+  onCancel,
+  edit,
+  setedit,
+  defaultEditData,
+  setDefaultEditData,
+}) => {
   const [knowledgeData, setKnowledgeData] = useState({
     type: "",
     sourceType: "",
@@ -11,32 +28,100 @@ const NewKnowledge = ({ onCancel }) => {
 
   const [imageUrl, setImageUrl] = useState("");
   const [videoUrl, setVideoUrl] = useState("");
+  const [isMediaChanged, setIsMediaChanged] = useState(false);
 
   useEffect(() => {
-    if (knowledgeData.mediaFile && knowledgeData.sourceType === "image") {
-      const reader = new FileReader();
-      reader.onload = (e) => {
-        setImageUrl(e.target.result);
-      };
-      reader.readAsDataURL(knowledgeData.mediaFile);
+    if (knowledgeData.mediaFile && knowledgeData.sourceType && isMediaChanged) {
+      const file = knowledgeData.mediaFile;
+
+      if (file instanceof Blob) {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+          if (knowledgeData.sourceType === "image") {
+            setImageUrl(e.target.result);
+          } else if (knowledgeData.sourceType === "video") {
+            setVideoUrl(e.target.result);
+          }
+        };
+        reader.readAsDataURL(file);
+      }
     }
-    if (knowledgeData.mediaFile && knowledgeData.sourceType === "video") {
-      const reader = new FileReader();
-      reader.onload = (e) => {
-        setVideoUrl(e.target.result);
-      };
-      reader.readAsDataURL(knowledgeData.mediaFile);
+  }, [knowledgeData.mediaFile, knowledgeData.sourceType, isMediaChanged]);
+
+  useEffect(() => {
+    if (edit) {
+      setKnowledgeData(defaultEditData);
     }
-  }, [knowledgeData.mediaFile]);
+  }, [edit, defaultEditData]);
+
+  const upload = async () => {
+    let url = null;
+
+    if (knowledgeData.mediaFile && !edit) {
+      try {
+        const mediaRef = ref(
+          storage,
+          `knowledge/${knowledgeData.mediaFile.name}`
+        );
+        await uploadBytes(mediaRef, knowledgeData.mediaFile);
+        url = await getDownloadURL(mediaRef);
+        console.log("Media uploaded to:", url);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+
+    if (!edit) {
+      const eventRef = collection(db, "knowledge");
+      try {
+        await addDoc(eventRef, { ...knowledgeData, mediaFile: url });
+        console.log("Added to database");
+      } catch (e) {
+        console.log(e);
+      }
+    } else {
+      if (isMediaChanged) {
+        try {
+          const mediaRef = ref(
+            storage,
+            `knowledge/${knowledgeData.mediaFile.name}`
+          );
+          await uploadBytes(mediaRef, knowledgeData.mediaFile);
+          url = await getDownloadURL(mediaRef);
+          setKnowledgeData((prevData) => ({ ...prevData, mediaFile: url }));
+          console.log("Media updated in:", url);
+        } catch (e) {
+          console.log(e);
+        }
+      }
+
+      const docRef = doc(db, "knowledge", knowledgeData.id);
+      const fieldsToExclude = !isMediaChanged ? ["mediaFile", "id"] : ["id"];
+      const knowledgeDataToUpdate = Object.fromEntries(
+        Object.entries({ ...knowledgeData, mediaFile: url }).filter(
+          ([key]) => !fieldsToExclude.includes(key)
+        )
+      );
+
+      try {
+        await updateDoc(docRef, knowledgeDataToUpdate);
+        console.log("Document updated:", knowledgeDataToUpdate);
+        onCancel();
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    onCancel();
+  };
 
   return (
-    <div className="absolute w-screen h-screen lg:w-full lg:h-full z-10 top-0 left-0  bg-[#50525580] flex justify-center items-center">
-      <div className="w-full lg:w-[90%] h-full md:h-fit bg-white rounded-md p-4  md:p-8">
-        <div className="w-full h-fit flex gap-4 justify-between flex-wrap mt-12">
+    <div className="absolute w-screen h-screen lg:w-full lg:h-full z-10 top-0 left-0 bg-[#50525580] flex justify-center items-center">
+      <div className="w-full lg:w-[90%] h-full md:h-fit bg-white rounded-md p-4 md:p-8">
+        <div className="flex flex-wrap justify-between w-full gap-4 mt-12 h-fit">
           <div className="w-full md:w-[50%] h-fit flex flex-col gap-7 ">
             <input
               type="text"
-              className="w-full h-10 pl-2 rounded-md border-none outline-none bg-slate-300 font-medium "
+              className="w-full h-10 pl-2 font-medium border-none rounded-md outline-none bg-slate-300"
               placeholder="Type"
               value={knowledgeData.type}
               onChange={(e) =>
@@ -45,7 +130,7 @@ const NewKnowledge = ({ onCancel }) => {
             />
             <input
               type="text"
-              className="w-full h-10 pl-2 rounded-md border-none outline-none bg-slate-300 font-medium "
+              className="w-full h-10 pl-2 font-medium border-none rounded-md outline-none bg-slate-300"
               placeholder="Source Type"
               value={knowledgeData.sourceType}
               onChange={(e) =>
@@ -61,25 +146,26 @@ const NewKnowledge = ({ onCancel }) => {
               <img
                 src={imageUrl}
                 alt="image"
-                className="absolute w-full h-full top-0 left-0 z-1 rounded-md object-cover"
+                className="absolute top-0 left-0 object-cover w-full h-full rounded-md z-1"
               />
             ) : (
-              <p className="text-gray-500 font-medium">Media Preview 🖼️</p>
+              <p className="font-medium text-gray-500">Media Preview 🖼️</p>
             )}
             {knowledgeData.mediaFile &&
               knowledgeData.sourceType === "video" && (
                 <video
                   src={videoUrl}
-                  alt="image"
-                  className="absolute w-full h-full top-0 left-0 z-1 rounded-md object-cover"
+                  alt="video"
+                  className="absolute top-0 left-0 object-cover w-full h-full rounded-md z-1"
+                  controls
                 />
               )}
           </div>
         </div>
-        <div className="w-full h-fit flex justify-between mt-9 md:mt-0 flex-wrap">
+        <div className="flex flex-wrap justify-between w-full h-fit mt-9 md:mt-0">
           <textarea
             className="w-full md:w-[50%] h-[15rem] outline-none border-none rounded-md bg-slate-300 p-3"
-            placeholder="description"
+            placeholder="Description"
             value={knowledgeData.description}
             onChange={(e) =>
               setKnowledgeData({
@@ -88,31 +174,34 @@ const NewKnowledge = ({ onCancel }) => {
               })
             }
           />
-          <div className=" w-full md:w-[40%] h-full flex flex-col gap-5 mt-5 ">
-            <div className="w-full h-10 pl-2 flex justify-center items-center rounded-md border-none outline-none bg-slate-400 font-medium relative">
+          <div className="w-full md:w-[40%] h-full flex flex-col gap-5 mt-5">
+            <div className="relative flex items-center justify-center w-full h-10 pl-2 font-medium border-none rounded-md outline-none bg-slate-400">
               <input
                 type="file"
-                accept={knowledgeData.sourceType === "image" ? ".jpg,.png" : ""}
-                className="w-full h-full absolute top-0 left-0 opacity-0 cursor-pointer"
-                onChange={(e) =>
-                  setKnowledgeData({
-                    ...knowledgeData,
-                    mediaFile: e.target.files[0],
-                  })
+                accept={
+                  knowledgeData.sourceType === "image" ? "image/*" : "video/*"
                 }
+                className="absolute top-0 left-0 w-full h-full opacity-0 cursor-pointer"
+                onChange={(e) => {
+                  const file = e.target.files[0];
+                  if (file) {
+                    setKnowledgeData((prevData) => ({
+                      ...prevData,
+                      mediaFile: file,
+                    }));
+                    setIsMediaChanged(true);
+                  }
+                }}
               />
-              <p className="text-gray-500 font-medium">Add Media + </p>
+              <p className="font-medium text-gray-500">Add Media + </p>
             </div>
             <input
               type="text"
-              className="w-full h-10 pl-2 rounded-md border-none outline-none bg-slate-200 font-medium "
+              className="w-full h-10 pl-2 font-medium border-none rounded-md outline-none bg-slate-200"
               placeholder="Link 🔗"
               value={knowledgeData.mediaUrl}
               onChange={(e) =>
-                setKnowledgeData({
-                  ...knowledgeData,
-                  mediaUrl: e.target.value,
-                })
+                setKnowledgeData({ ...knowledgeData, mediaUrl: e.target.value })
               }
             />
           </div>
@@ -124,8 +213,11 @@ const NewKnowledge = ({ onCancel }) => {
           >
             <p>Cancel</p>
           </div>
-          <div className="w-[9rem] h-full rounded-md bg-blue-500 flex items-center justify-center cursor-pointer shadow-md shadow-blue-100">
-            <p className="text-white font-medium">Upload Mentor</p>
+          <div
+            className="w-[9rem] h-full rounded-md bg-blue-500 flex items-center justify-center cursor-pointer shadow-md shadow-blue-100"
+            onClick={() => upload()}
+          >
+            <p className="font-medium text-white">Upload</p>
           </div>
         </div>
       </div>
