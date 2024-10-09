ec96518ea91cd4be54d6338d451b79e2a6f1751f
PRINCE ROY <83387075+princeroy8606@users.noreply.github.com>
2024-10-09 14:14:49 +0530

    Update JoinCommunity.jsx

--- src/components/Landing/Components/JoinCommunity.jsx
+++ src/components/Landing/Components/JoinCommunity.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const JoinCommunity = () => {
+  const [openPopup, setOpenPopup] = useState(false)
+
+  const openNewWindow = (link) => {
+    window.open(link, "_blank")
+    openPopup && setOpenPopup(false)
+  }
   return (
     <section
       id="join-community"
       className="flex flex-col items-center justify-center h-full p-3 mx-auto overflow-hidden bg-white cursor-default md:h-full xl:h-[60vh] xl:py-0 xl:px-20 max-w-7xl sm:pb-3 md:pb-5"
     >
+      {
+        openPopup && <div className='absolute w-[21rem] h-[17rem] bg-[#1850A8] z-20 rounded-md  shadow-lg p-4 '>
+          <p className='text-gray-300 text-[1rem]'>Join the students community only after receiving community badge</p>
+          <p className='text-white text-[1.4rem] font-medium mt-4'>Have you received your community badge ?</p>
+
+          <div className='w-full flex items-center justify-between mt-10'>
+            <div
+              onClick={() => openNewWindow('https://chat.whatsapp.com/Gnfxll2wN3Y5Mg8kJowpAj')}
+              className='w-[5rem] h-[2.5rem] rounded-md bg-white hover:text-white hover:bg-[#f26d26] shadow-md flex items-center justify-center cursor-pointer'>
+              <p className='font-normal hover:font-semibold'>YES</p>
+            </div>
+            <div
+              onClick={() => openNewWindow('https://wowhr.in/badge/')}
+              className='w-[5rem] h-[2.5rem] rounded-md bg-white hover:text-white hover:bg-[#f26d26] shadow-md flex items-center justify-center cursor-pointer'>
+              <p>NO</p>
+            </div>
+          </div>
+        </div>
+      }
+
       <div
         className="mb-3 text-xl font-semibold xl:mt-0 md:mt-10 xl:mb-8 md:text-4xl xl:text-3xl font-jost"
         data-aos="fade-right"
@@ -18,29 +44,31 @@ const JoinCommunity = () => {
         data-aos="fade-left"
         data-aos-duration="1000"
       >
-        All HR / Management students can be part of WOW HR Community. Follow the below instructions:                                                                                                                          
-        You can fill your LinkedIn details by clicking on the "Join Our Community" button below and generate your complimentary WOW HR membership badge.                                      
+        All HR / Management students can be part of WOW HR Community. Follow the below instructions:
+        You can fill your LinkedIn details by clicking on the "Join Our Community" button below and generate your complimentary WOW HR membership badge.
         Post your badge on LinkedIn and tag WOW HR. Also join our whatsapp group.
       </div>
-      
+
       <div className="flex space-x-4">
         <a
-          href="https://wowhr.in/badge/"
-          target="_blank"
+          // href="https://wowhr.in/badge/"
+          // target="_blank"
           rel="noopener noreferrer"
-          className="bg-darkBlue font-jost text-white text-[.6rem] md:text-base xl:text-sm xl:hover:shadow-lg px-2 md:px-8 xl:px-6 min-w-24 xl:min-w-36 py-2 xl:py-3 font-medium rounded-md xl:hover:bg-[#089adec1] xl:hover:text-white transition-all duration-200 xl:hover:scale-[1.02] xl:hover:translate-y-[-.1rem]"
+          className="bg-darkBlue font-jost text-white bg-[#089ade99] text-[.6rem] md:text-base xl:text-sm xl:hover:shadow-lg px-2 md:px-8 xl:px-6 min-w-24 xl:min-w-36 py-2 xl:py-3 font-medium rounded-md xl:hover:bg-[#089adec1] xl:hover:text-white transition-all duration-200 xl:hover:scale-[1.02] xl:hover:translate-y-[-.1rem]"
           data-aos="zoom-in"
           data-aos-duration="1000"
+          onClick={() => openNewWindow('https://wowhr.in/badge/')}
         >
           Join Our Community
         </a>
         <a
-          href="https://chat.whatsapp.com/Gnfxll2wN3Y5Mg8kJowpAj"
-          target="_blank"
+          // href="https://chat.whatsapp.com/Gnfxll2wN3Y5Mg8kJowpAj"
+          // target="_blank"
           rel="noopener noreferrer"
-          className="bg-darkBlue font-jost text-white text-[.6rem] md:text-base xl:text-sm xl:hover:shadow-lg px-2 md:px-8 xl:px-6 min-w-24 xl:min-w-36 py-2 xl:py-3 font-medium rounded-md xl:hover:bg-[#089adec1] xl:hover:text-white transition-all duration-200 xl:hover:scale-[1.02] xl:hover:translate-y-[-.1rem]"
+          className="bg-darkBlue font-jost text-white bg-[#089ade99] text-[.6rem] md:text-base xl:text-sm xl:hover:shadow-lg px-2 md:px-8 xl:px-6 min-w-24 xl:min-w-36 py-2 xl:py-3 font-medium rounded-md xl:hover:bg-[#089adec1] xl:hover:text-white transition-all duration-200 xl:hover:scale-[1.02] xl:hover:translate-y-[-.1rem]"
           data-aos="zoom-in"
           data-aos-duration="1000"
+          onClick={() => setOpenPopup(true)}
         >
           Join Student Community
         </a>
